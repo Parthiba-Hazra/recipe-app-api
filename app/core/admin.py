bbd8ba7c107e01d1e0c8c9b7a1bcbdc1a7397a4e
Parthiba-Hazra <parthibahazra02@gmail.com>
2023-04-21 15:51:54 +0530

    add user test cases
    Signed-off-by: Parthiba-Hazra <parthibahazra02@gmail.com>

--- app/core/admin.py
+++ app/core/admin.py
@@ -15,10 +15,11 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
-                'fields':(
+                'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
@@ -39,7 +40,7 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
 
